206c2d46ad7b552cf4e33e89a01f26c1caad2bcc
xlxs4 <xlxs4@protonmail.ch>
2019-07-22 18:06:44 +0300

    Add read/write csv functions

--- app/parser/reader.py
+++ app/parser/reader.py
@@ -1,1 +1,7 @@
 import csv
+
+def read(filename):
+	with open(filename, newline='') as books_list:
+		books_reader = csv.reader(books_list, delimiter=' ', quotechar='|')
+		for row in books_reader:
+			print(', '.join(row))
--- app/parser/writer.py
+++ app/parser/writer.py
@@ -1,1 +1,6 @@
 import csv
+
+def write(filename):
+	with open(filename, 'a', newline='') as books_list:
+		books_writer = csv.writer(books_list, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		books_writer.writerow(["I am a title"] + ["I'm an author"] + ["I am a year"] + ["I am a publisher"])
