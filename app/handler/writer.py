33598c8f1ac028fd8bb814abd3efdb003677312c
xlxs4 <xlxs4@protonmail.ch>
2019-07-24 20:52:52 +0300

    Finish write

--- app/handler/writer.py
+++ app/handler/writer.py
@@ -1,10 +1,6 @@
 import csv
 
-def write(filename, **kwargs):
-	title = kwargs.get('title', "None")
-	author = kwargs.get('author', "None")
-	year = kwargs.get('year', "None")
-	publisher = kwargs.get('publisher', "None")
+def write(filename, rows):
 	with open(filename, 'a', newline='') as books_list:
-		books_writer = csv.writer(books_list, quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		books_writer.writerow([title] + [author] + [year] + [publisher])
+		books_writer = csv.writer(books_list, quotechar='"', quoting=csv.QUOTE_MINIMAL, escapechar=' ')
+		books_writer.writerows(rows)
